18473ade4b6de2d3b8b526e123af2def1f6d3af6
Deja7 <dawid7.deja@gmail.com>
2022-10-11 00:01:18 +0200

    imported sfml

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(800, 800), "sandbox");
 
 
+	while (!sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
+	{
+		window.clear(sf::Color::Black);
+
+		window.display();
+	}
+
+
+	return 0;
 }
